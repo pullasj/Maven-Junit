028d8349f08472e69d821a865531ea7d28e74a4c
Surya Sarath Pulla <155488064+pullasj@users.noreply.github.com>
2025-02-21 14:25:42 -0500

    Update TestNG.java

--- src/test/java/TestNG.java
+++ src/test/java/TestNG.java
@@ -1,26 +1,37 @@
 import org.testng.annotations.Test;
 
-public class TestNG extends File1{
+public class TestNG extends File1 {
 
-    @Test(priority = 0 )
-    public void validateLogin(){
+    // This test is executed first due to priority = 0
+    @Test(priority = 0)
+    public void validateLogin() {
+        // This method validates the page title by calling the ValidateTitle method from File1
         ValidateTitle();
     }
 
-    @Test(priority = 1 )
-    public void Login(){
+    // This test is executed second due to priority = 1
+    @Test(priority = 1)
+    public void Login() {
+        // This method validates login credentials by calling the ValidateLogin method from File1
         ValidateLogin();
     }
 
-    @Test(priority = 2 )
-    public void TT(){
+    // Test with no implementation yet (executed after Login due to default priority)
+    @Test(priority = 2)
+    public void TT() {
+        // Add test code here
     }
 
+    // Another test with no implementation yet
     @Test
-    public void Te1(){
+    public void Te1() {
+        // Add test code here
     }
 
+    // This test depends on the successful execution of Te1
     @Test(dependsOnMethods = {"Te1"})
-    public void Te2(){
+    public void Te2() {
+        // This test will only run if Te1 is successful
+        // Add test code here
     }
 }
