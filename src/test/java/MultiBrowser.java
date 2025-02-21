9211a548b21668709db5c5fb15f163f39db4ec01
Surya Sarath Pulla <155488064+pullasj@users.noreply.github.com>
2025-02-21 12:53:42 -0500

    Update MultiBrowser.java

--- src/test/java/MultiBrowser.java
+++ src/test/java/MultiBrowser.java
@@ -12,43 +12,49 @@ import org.testng.annotations.Test;
 public class MultiBrowser {
     public WebDriver driver;
 
+    // Accepts a browser parameter from testng.xml to determine which browser to launch
     @Parameters("Browser")
     @BeforeClass
     public void setUp(String Browser) {
+        // Checking the passed browser parameter and initializing the respective WebDriver
         if (Browser.equalsIgnoreCase("Chrome")) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            WebDriverManager.chromedriver().setup(); // Sets up ChromeDriver using WebDriverManager
+            driver = new ChromeDriver(); // Launches Chrome browser
         } else if (Browser.equalsIgnoreCase("Firefox")) {
-            WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
+            WebDriverManager.firefoxdriver().setup(); // Sets up FirefoxDriver using WebDriverManager
+            driver = new FirefoxDriver(); // Launches Firefox browser
         } else {
-            WebDriverManager.edgedriver().setup();
-            driver = new EdgeDriver();
+            WebDriverManager.edgedriver().setup(); // Sets up EdgeDriver using WebDriverManager
+            driver = new EdgeDriver(); // Launches Edge browser
         }
     }
 
+    // Closes the browser after test execution
     @AfterClass
     public void tearDown() {
         if (driver != null) {
-            driver.quit();
+            driver.quit(); // Closes all browser windows and ends the WebDriver session
         }
     }
 
+    // Smoke test case executed when running on Chrome
     @Test(groups = {"smoke"})
     public void test1() {
-        System.out.println("From Chrome");
+        System.out.println("Executing test1 on Chrome");
         // Add your test code here
     }
 
+    // Another smoke test case executed when running on Chrome
     @Test(groups = {"smoke"})
     public void test2() {
-        System.out.println("From Chrome");
+        System.out.println("Executing test2 on Chrome");
         // Add your test code here
     }
 
+    // Regression test case executed when running on Edge
     @Test(groups = {"regression"})
     public void test3() {
-        System.out.println("From Edge");
+        System.out.println("Executing test3 on Edge");
         // Add your test code here
     }
 }
