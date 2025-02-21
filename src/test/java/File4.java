b7ea5ed7833180190d7f6675c7c011151c527969
Surya Sarath Pulla <155488064+pullasj@users.noreply.github.com>
2025-02-21 12:51:31 -0500

    Update File4.java

--- src/test/java/File4.java
+++ src/test/java/File4.java
@@ -4,53 +4,85 @@ import org.openqa.selenium.interactions.Actions;
 import java.util.Set;
 
 public class File4 extends File1 {
+
     @Test
-    public void ActionClass(){
+    public void ActionClass() {
+        // Clear the username field before entering data
         driver.findElement(By.id("user-name")).clear();
+
+        // Using Actions class for advanced user interactions
         Actions actions = new Actions(driver);
         WebElement element = driver.findElement(By.id("user-name"));
-        actions.moveToElement(element).click(element).sendKeys("standard_user").doubleClick().contextClick(element).build().perform();
+
+        // Performing a series of actions:
+        // 1. Move to the element
+        // 2. Click on it
+        // 3. Type 'standard_user'
+        // 4. Double-click the text
+        // 5. Right-click on the element (context click)
+        actions.moveToElement(element)
+                .click(element)
+                .sendKeys("standard_user")
+                .doubleClick()
+                .contextClick(element)
+                .build()
+                .perform();
     }
 
     @Test
-    public void UserAlert(){
-    driver.navigate().to("https://only-testing-blog.blogspot.com/2014/01/textbox.html?");
+    public void UserAlert() {
+        // Navigating to a test page that contains alert popups
+        driver.navigate().to("https://only-testing-blog.blogspot.com/2014/01/textbox.html?");
 
-    driver.findElement(By.xpath("//input[@value='Show Me Alert']")).isDisplayed();
-    driver.findElement(By.xpath("//input[@value='Show Me Alert']")).click();
-    // Ok on the alert
-    driver.switchTo().alert().accept();
+        // Finding and clicking the "Show Me Alert" button
+        driver.findElement(By.xpath("//input[@value='Show Me Alert']")).isDisplayed();
+        driver.findElement(By.xpath("//input[@value='Show Me Alert']")).click();
+        
+        // Accepting the alert popup
+        driver.switchTo().alert().accept();
 
-    driver.findElement(By.xpath("//button[text()='Show Me Prompt']")).isDisplayed();
-    driver.findElement(By.xpath("//button[text()='Show Me Prompt']")).click();
-    // Sending Message
-    driver.switchTo().alert().sendKeys("Surya Test manager");
-    // Getting Alert into string
-    String Alertmsg = driver.switchTo().alert().getText();
-    System.out.println(Alertmsg);
-    driver.switchTo().alert().dismiss();
+        // Finding and clicking the "Show Me Prompt" button
+        driver.findElement(By.xpath("//button[text()='Show Me Prompt']")).isDisplayed();
+        driver.findElement(By.xpath("//button[text()='Show Me Prompt']")).click();
+
+        // Sending input text into the alert prompt
+        driver.switchTo().alert().sendKeys("Surya Test manager");
+
+        // Capturing the alert message
+        String Alertmsg = driver.switchTo().alert().getText();
+        System.out.println("Alert Message: " + Alertmsg);
+
+        // Dismissing the alert instead of accepting it
+        driver.switchTo().alert().dismiss();
     }
 
     @Test
-    public void cookie(){
+    public void cookie() {
+        // Logging in before handling cookies
         ValidateLogin();
 
-        //Storing Cookies
+        // Retrieving and printing all cookies stored in the browser
         Set<Cookie> cookies = driver.manage().getCookies();
-        System.out.println(cookies);
+        System.out.println("All Cookies: " + cookies);
 
-        //TO get Single Cookie
+        // Retrieving a specific cookie by name
         Cookie user = driver.manage().getCookieNamed("session-username");
-        System.out.println(user);
+        System.out.println("Session Cookie: " + user);
 
-        // Deleting specific Cookie
+        // Deleting a specific cookie using object reference
         driver.manage().deleteCookie(user);
+
+        // Deleting a specific cookie by its name
         driver.manage().deleteCookieNamed("session-username");
-        // Deleting all the cookies
+
+        // Deleting all cookies stored in the browser
         driver.manage().deleteAllCookies();
 
-        Cookie custom = new Cookie("password","pass");
+        // Creating and adding a custom cookie to the browser
+        Cookie custom = new Cookie("password", "pass");
         driver.manage().addCookie(custom);
-        System.out.println(driver.manage().getCookieNamed("password"));
+
+        // Retrieving and printing the custom cookie
+        System.out.println("Custom Cookie: " + driver.manage().getCookieNamed("password"));
     }
 }
